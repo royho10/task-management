8fc5070f16a2e4a8914fee0a70cd79c453c4bce1
Ran Seroussi <ran.saroussi15@gmail.com>
2021-07-14 23:45:02 +0300

    empty

--- src/components/Navigation/Navigation.test.js
+++
@@ -1,8 +0,0 @@
-import { shallow } from 'enzyme';
-import React from 'react';
-import Navigation from './Navigation';
-
-it('expect to render Navigation component', () => {
-	
-	expect(shallow(<Navigation />)).toMatchSnapshot();
-})
